13bb4b92f524f95e7def4407b45aec53d9e90ece
CodeTheory <hello@timothy.is>
2016-11-29 23:20:02 +0000

    Make linter happy

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -466,7 +466,7 @@ ipcMain.on('set-main-window-visibility', (event, opts) => {
 
 ipcMain.on('toggle-maximize-editor-window', () => {
   if (!postRecWindow) {
-    return
+    return;
   }
   if (postRecWindow.isMaximized()) {
     postRecWindow.unmaximize();
